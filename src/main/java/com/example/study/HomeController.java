f346b6d33b34f8721f98fa3e068c7229f93e9cd3
SC <pch838811@gmail.com>
2021-05-30 17:20:47 +0900

    init thymeleaf

---
+++ src/main/java/com/example/study/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.study;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import reactor.core.publisher.Mono;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public Mono<String> home() {
+		return Mono.just("home");
+	}
+}
