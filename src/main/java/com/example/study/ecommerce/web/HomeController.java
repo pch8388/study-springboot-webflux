1190982b181a2303f0a53da23bd07f0b053bd976
SC <pch838811@gmail.com>
2021-06-01 23:40:49 +0900

    Service 로 로직 이동 & 오류 수정

--- src/main/java/com/example/study/ecommerce/web/HomeController.java
+++ src/main/java/com/example/study/ecommerce/web/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 
+import com.example.study.ecommerce.application.CartService;
 import com.example.study.ecommerce.domain.Cart;
-import com.example.study.ecommerce.domain.CartItem;
 import com.example.study.ecommerce.domain.CartRepository;
 import com.example.study.ecommerce.domain.ItemRepository;
 
@@ -18,6 +18,7 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class HomeController {
 
+	private final CartService cartService;
 	private final ItemRepository itemRepository;
 	private final CartRepository cartRepository;
 
@@ -32,23 +33,7 @@ public class HomeController {
 
 	@PostMapping("/add/{id}")
 	public Mono<String> addToCart(@PathVariable String id) {
-		return this.cartRepository.findById("My Cart")
-			.defaultIfEmpty(new Cart("My Cart"))
-			.flatMap(cart -> cart.getCartItems().stream()
-				.filter(cartItem -> cartItem.getItem().getId().equals(id))
-				.findAny()
-				.map(cartItem -> {
-					cartItem.increment();
-					return Mono.just(cart);
-				})
-				.orElseGet(() -> this.itemRepository.findById(id)
-					.map(CartItem::new)
-					.map(cartItem -> {
-						cart.getCartItems().add(cartItem);
-						return cart;
-					})
-				))
-			.flatMap(this.cartRepository::save)
+		return cartService.addToCart("My Cart", id)
 			.thenReturn("redirect:/");
 	}
 }
