13dc48c37ba5dce2e8bf8da2af8c940bf7c91477
SC <pch838811@gmail.com>
2021-06-04 23:12:18 +0900

    logging 을 위한 블록하운드 설정
    - block hound 는 blocking 한 로직이 있다면 에러를 발생시킨다
    - 타임리프는 blocking code 이기 때문에 예외가 발생한다 (RandomAccessFile.readBytes 메서드)

--- src/main/java/com/example/study/StudySpringbootWebfluxApplication.java
+++ src/main/java/com/example/study/StudySpringbootWebfluxApplication.java
@@ -3,10 +3,14 @@ package com.example.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.blockhound.BlockHound;
+
 @SpringBootApplication
 public class StudySpringbootWebfluxApplication {
 
 	public static void main(String[] args) {
+		BlockHound.install();
+
 		SpringApplication.run(StudySpringbootWebfluxApplication.class, args);
 	}
 
